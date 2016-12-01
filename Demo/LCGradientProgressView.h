2172c3834b438266fc09456ee5355d5c210fcbd2
luocheng <1059326760@qq.com>
2016-12-01 11:44:07 +0800

    simple gradient progressView demo

---
+++ Demo/LCGradientProgressView.h
@@ -0,0 +1,13 @@
+//
+//  LCGradientProgressView.h
+//  Demo
+//
+//  Created by 罗成 on 16/11/30.
+//  Copyright © 2016年 LuoCheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCGradientProgressView : UIView
+
+@end
---
+++ Demo/LCGradientProgressView.m
@@ -0,0 +1,21 @@
+//
+//  LCGradientProgressView.m
+//  Demo
+//
+//  Created by 罗成 on 16/11/30.
+//  Copyright © 2016年 LuoCheng. All rights reserved.
+//
+
+#import "LCGradientProgressView.h"
+
+@implementation LCGradientProgressView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
